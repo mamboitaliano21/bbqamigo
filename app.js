528f1530221ab044380199515deeea6e0bbe08fe
zelfrontial <denisthamrin@gmail.com>
2014-08-08 23:15:43 +0700

    Full Functionallity Done Only need design and javascript
    YEAH

--- app.js
+++ app.js
@@ -10,6 +10,55 @@ var users = require('./routes/users');
 
 var app = express();
 
+//CouchDB dependencies
+var cradle = require('cradle');
+
+//Connect to Denis' Laptop DB
+//TODO change it to production
+var c = new(cradle.Connection)('10.0.8.184', 5984, {
+      cache: true,
+      raw: false,
+      forceSave: true
+  });
+var db = c.database('freefood');
+db.exists(function (err, exists) {
+	if (err) {
+      console.log('error', err);
+    } else if (exists) {
+      console.log('the force is with you.');
+    } else {
+      console.log('database does not exists.');
+      db.create();
+      /* populate design documents */
+    }
+  });
+
+// app.locals.submitEvent = $(function() {
+	// $('#registerEvent').submit(function(event) {
+		// event.preventDefault(); // Stops browser from navigating away from page
+		// var data;
+		////build a json object or do something with the form, store in data
+		// $.post('/registerEvent', data, function(resp) {
+			// alert(resp);
+			////do something when it was successful
+		// });
+	// });
+// });			
+
+
+
+// var res = db.view('freeFoodViews/getEventsByUni', function (err, res) {
+	  // console.log(res);
+	  // res.forEach(function (row) {
+         // console.log("{Uni: %s,Time: %s,Location: %s,Organizer: %s,Description: %s,Picture: }",row.Uni,row.Time,row.Location,row.Organizer,row.Description);
+      // });
+	  // console.log(res);
+  // });
+  
+
+
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -21,10 +70,16 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req,res,next){
+	req.db = db;
+	next();
+});
+
 app.use('/', routes);
 app.use('/users', users);
 
-/// catch 404 and forward to error handler
+
+// catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,39 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'BBQ Amigo' });
+  var db = req.db
+  var json1 = {}
+  db.view('freeFoodViews/getEventsByUni', function (err, response) {
+
+	//var jpost = JSON.stringify(res.rows);
+	var json = response.rows
+	
+	res.render('index', {jsonx: json});		
+	});
 });
+router.get('/eventList/:uni', function(req, res) {
+	var db = req.db
+    var uni = req.params.uni
+    db.view('freeFoodViews/getEventsByUni', {key: uni}, function (err, doc) {
+//		console.log(doc)
+		var json = doc.rows
+		res.render('eventList', {jsonx: json});	
+	});
+	//var jpost = JSON.stringify(res.rows);
+	//res.render('helloworld', { title: 'Hello, World!' })
+});	
+
+
+	
+	//var jpost = JSON.stringify(res.email);
+	//res.render('index', {post : jpost});
+  // });
+	
+	//console.log(jpost);
+router.post('/registerEvent', function(req,res){
+	var db = req.db
+	db.save(req.body)
+	console.log(req.body);
+})
 
 module.exports = router;
